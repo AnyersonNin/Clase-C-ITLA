d3e541ccbc614e1041a101c3a86ec8f1245a3597
Anyerson Nin <anyerson.nin@gabsocial.gob.do>
2024-09-23 23:36:18 -0400

    Trabajo Final ITLA C# Basico

--- Trabajo Final ITLA C#basico/ShopWeb/ShopWeb/obj/Debug/net8.0/ShopWeb.AssemblyInfo.cs
+++ Trabajo Final ITLA C#basico/ShopWeb/ShopWeb/obj/Debug/net8.0/ShopWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de0dd445b437b763a59a7e7b0f821a901ab7019a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c44c0f7c3432c4667916276346735fbe1f908a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
