6f2be25f8eb8fb94dda53e48ffb3200c6b5cebab
Anyerson Nin <anyerson.nin@gabsocial.gob.do>
2025-03-07 13:50:20 -0400

    Desarrollo de la Etapa 7: Configuración de SignalR

--- ITLA C# Avanzado/GestorTareas/GestorTareas/Program.cs
+++ ITLA C# Avanzado/GestorTareas/GestorTareas/Program.cs
@@ -54,7 +54,7 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
     options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
